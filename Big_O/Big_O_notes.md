ec0ca0465ef9c41055c6c5aa4e2f3e7cf6a93928
charles <codelyfewithcharlie@gmail.com>
2021-01-02 04:49:02 -0500

    updates

--- Big_O/Big_O_notes.md
+++ Big_O/Big_O_notes.md
@@ -5,4 +5,12 @@
 - readable
 - scalable
 
-#
+### O(n)
+
+- linear time
+- time grows linear as the input grows
+
+### O(1)
+
+- constant time
+- fast, happens immediately
--- Big_O/index.js
+++ Big_O/index.js
@@ -4,14 +4,11 @@ const nemo = ['nemo', 'dory', 'a', 'b', 'c', 'd'];
 const large = new Array(1000).fill('nemo');
 
 function findNemo(arr) {
-  const t0 = performance.now();
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === 'nemo') {
       console.log('Found Nemo');
     }
   }
-  const t1 = performance.now();
-  console.log(t1 - t0);
 }
 
 findNemo(large);
