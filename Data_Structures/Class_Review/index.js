98b2b5acab010c81b4ad31e34fa61cfd5de482cd
charles <codelyfewithcharlie@gmail.com>
2021-01-06 08:44:40 -0500

    updates

---
+++ Data_Structures/Class_Review/index.js
@@ -0,0 +1,22 @@
+class Player {
+  constructor(name, type) {
+    this.name = name;
+    this.type = type;
+  }
+  sayHello() {
+    console.log(`Hi, I'm am ${this.name}`);
+    return;
+  }
+}
+
+class Guitarist extends Player {
+  constructor(name, type) {
+    super(name, type);
+  }
+  shred() {
+    console.log(`I am ${this.name} the guitar player`);
+  }
+}
+
+const charlie = new Guitarist('charlie', 'guy');
+charlie.shred();
