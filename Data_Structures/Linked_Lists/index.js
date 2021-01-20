f926e48a2d50737b0f76971189ef4d8c65413915
charles <codelyfewithcharlie@gmail.com>
2021-01-20 16:11:33 -0500

    linkedlist practice

---
+++ Data_Structures/Linked_Lists/index.js
@@ -0,0 +1,51 @@
+// 10 >> 5 >> 16
+
+// let myLinkedList = {
+//   head: {
+//     value: 10,
+//     next: {
+//       value: 5,
+//       next: {
+//         value: 16,
+//         next: null,
+//       },
+//     },
+//   },
+// };
+
+class NewNode {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    this.head = { value, next: null };
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  append(value) {
+    const newNode = new NewNode(value);
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length += 1;
+    return this;
+  }
+
+  prepend(value) {
+    const newNode = new NewNode(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length += 1;
+    return this;
+  }
+}
+
+const myLinkedList = new LinkedList(10);
+myLinkedList.append(5);
+myLinkedList.prepend(2);
+
+console.log(myLinkedList);
